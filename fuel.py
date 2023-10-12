a4b399ab5d04b815f158c18dbc1fa7540de61911
Mkhabele Mikejunior Mark Mashaole <137772011+mkhmik004@users.noreply.github.com>
2023-10-12 13:48:34 +0200

    Update fuel.py

--- fuel.py
+++ fuel.py
@@ -1,36 +1,29 @@
 #MKHABELE MMM
 #06/07/2023
 def main():
-        while True:
+    while True:
             try:
-                f=input("Fraction: ")
-                x,y=f.split("/")
-                if y==0:
-                        raise ZeroDivisionError
+                fraction=input("Fraction: ")
+                x,y=fraction.split("/")
+                x=int(x)
+                y=int(y)
+
+
+                percentage=round((x/y)*100)
+                if 1<percentage<99:
+                     print(f"{percentage}%")
+                     break
+                if 0<=percentage<=1:
+                     print('E')
+                     break
+                if 99<=percentage<=100:
+                     print("F")
+                     break
                 else:
-                       print(gauge(convert(f)))
-                       print(convert(f))
+                     continue
             except ZeroDivisionError as e:
                  continue
             except ValueError as e:
                  continue
-            
-def convert(fraction):
-                      x,y=fraction.split("/")
-                      x=int(x)
-                      y=int(y)
-                      p=round((x/y)*100)
-                      return p
 
-def gauge(percentage):
-                if 1<percentage<99:
-                     return (f"{percentage}%")
-
-                if 0<=percentage<=1:
-                     return ('E')
-
-                if 99<=percentage<=100:
-                     return ("F")
-                
-if __name__=="__main__":
-      main()
+main()
